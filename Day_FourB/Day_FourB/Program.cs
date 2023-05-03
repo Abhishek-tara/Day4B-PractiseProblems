21cb6a7d5bd1fd0eb2da6d45ea2f8a051ca09646
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-05-03 16:26:49 +0530

    Add: Method to count Duplicates

---
+++ Day_FourB/Day_FourB/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day_FourB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1
+            CountDuplicate countDuplicate = new CountDuplicate();
+            countDuplicate.CountMethod();
+        }
+    }
+}
